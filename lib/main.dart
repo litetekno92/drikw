cf6b60720d83744891bb4f2bc276ebd2c04b02af
LiteTekno92 <lite.tekno92@gmail.com>
2019-04-18 15:33:18 +0200

    Fontawesone added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,20 +45,27 @@ class MyHomePage extends StatelessWidget {
               child: Column(
             children: <Widget>[
               Material(
-                borderRadius: BorderRadius.all( Radius.circular(50.0) ),
-                elevation: 10,
-                child: Padding(padding: EdgeInsets.all(8.0),
-                child: Image.asset('images/mediasen-logo.png',width:92,height:92))
-              ),
-              Text('Flutter',style: TextStyle(fontSize: 12.0, color: Colors.white)),
+                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                  elevation: 10,
+                  child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Image.asset('images/mediasen-logo.png',
+                          width: 92, height: 92))),
+              Text('Flutter',
+                  style: TextStyle(fontSize: 12.0, color: Colors.white)),
             ],
           )),
         ),
-       
-        CustomListTile(Icons.home, 'Home', () => {}),
-        CustomListTile(Icons.person, 'Profile', () => {}),
-        CustomListTile(Icons.settings, 'Settings', () => {}),
-        
+
+        // CustomListTile(Icons.home, 'Home', () => {}),
+        // CustomListTile(Icons.person, 'Profile', () => {}),
+        // CustomListTile(Icons.settings, 'Settings', () => {}),
+
+        CustomListTile(icon: categories[0].icon, text: categories[0].title, onTap: () => {}),
+        CustomListTile(icon: categories[1].icon, text: categories[1].title, onTap: () => {}),
+        CustomListTile(icon: categories[2].icon, text: categories[2].title, onTap: () => {}),
+        CustomListTile(icon: categories[3].icon, text: categories[3].title, onTap: () => {}),
+        CustomListTile(icon: categories[4].icon, text: categories[4].title, onTap: () => {}),
       ])),
       body: Center(
         child: Column(
@@ -77,7 +85,7 @@ class CustomListTile extends StatelessWidget {
   final IconData icon;
   final String text;
   final Function onTap;
-  CustomListTile(this.icon, this.text, this.onTap);
+  CustomListTile({this.icon, this.text, this.onTap});
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -111,3 +119,57 @@ class CustomListTile extends StatelessWidget {
             )));
   }
 }
+
+class Category {
+  final int id;
+  final String title;
+  final ColorSwatch color;
+  final IconData icon;
+
+  /// Creates a [Category].
+  ///
+  /// A [Category] saves the title of the Category (e.g. 'Length'), its color for
+  /// the UI, and the icon that represents it (e.g. a ruler).
+  // While the  checks for whether a titled parameter is passed in,
+  // it doesn't check whether the object passed in is null. We check that
+  // in the assert statement.
+  Category({
+    this.id,
+    this.title,
+    this.color,
+    this.icon,
+  });
+}
+
+List<Category> categories = [
+  Category(
+    id: 1,
+    title: "ACCUEIL",
+    color: Colors.red,
+    icon: FontAwesomeIcons.home,
+  ),
+  Category(
+    id: 2,
+    title: "ECONOMIE",
+    color: Colors.red,
+    icon: FontAwesomeIcons.landmark  
+    ),
+  Category(
+    id: 3,
+    title: "SCIENCE & HIGH-TECH",
+    color: Colors.red,
+    icon: FontAwesomeIcons.atom
+  ),
+  Category(
+    id: 4,
+    title: "SPORTS",
+    color: Colors.red,
+    icon: FontAwesomeIcons.running,
+  ),
+  Category(
+      id: 5,
+      title: "ARTS & CULTURE",
+      color: Colors.red,
+      icon: FontAwesomeIcons.artstation
+      )
+];
